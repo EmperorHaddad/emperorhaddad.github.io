ade85744d23d896bb033b1f6b8cca8e0add82e52
EmperorHaddad <107798257+EmperorHaddad@users.noreply.github.com>
2023-09-17 13:51:49 +0100

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -141,11 +141,11 @@ const config = {
 
     // Custom theme
     customTheme: {
-      primary: '#fc055b',
-      secondary: '#219aaf',
-      accent: '#e8d03a',
-      neutral: '#2A2730',
-      'base-100': '#E3E3ED',
+      primary: '#181818',
+      secondary: '#121212',
+      accent: '#3D3D3D',
+      neutral: '#FFFFFF',
+      'base-100': '#AAAAAA',
       '--rounded-box': '3rem',
       '--rounded-btn': '3rem',
     },
